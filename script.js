c9fd429e4fc5661cd6e88a40723fac8b39107875
Rotwo <87866939+Rotwo@users.noreply.github.com>
2022-06-13 18:56:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// When inizialice the page, the script will load the maintenance page
+// And do nothing else
+
+// Load the maintenance page (./ext/maintenance.html)
+window.location.href = "./ext/maintenance.html";
