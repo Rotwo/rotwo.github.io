374939d8582d3a399c0f92428d2693de166d233d
Rotwo <87866939+Rotwo@users.noreply.github.com>
2022-06-14 17:41:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
 // And do nothing else
 
 // Load the maintenance page (./ext/maintenance.html)
-// Disable: 
-window.location.href = "./ext/maintenance.html";
+// Disable: window.location.href = "./ext/maintenance.html";
